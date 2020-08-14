7d026a9dd5df88ffdb0192ea139cf7068e392d85
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-08-14 12:46:37 -0500

    got ring buffer down to 2 fails and 2 errors

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,23 +2,17 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity  # max size it can be
         self.data = []  # holds the data
-
-    class __Full:  # if Full
-        def append(self, item):
-            self.data[self.cur] = item
-            self.cur = (self.cur+1) % self.capacity
-
-        def get(self):
-            # ask TL about this syntax (What is it?)
-            # return elements in list in correct order
-            return self.data[self.cur:] + self.data[:self.cur]
+        self.cur = 0
 
     def append(self, item):
+
         self.data.append(item)  # add item
         if len(self.data) == self.capacity:  # if at max capacity
-            self.cur = 0  # current
-            # change class to full
-            self.__class__ = self.__Full
+            self.data.pop(0)
+            self.data[self.cur] = item  # replace with item
+            # next will replace next index of list
+
+            self.cur = (self.cur+1) % self.capacity
 
     def get(self):
         return self.data  # return the list of elements of not full
