d6b56b1d957b2d5e9c26498fe8c63b4e341ef82c
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-08-14 14:20:46 -0500

    Ring buffer passes all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,3 +1,18 @@
+# class RingBuffer:
+#     def __init__(self, capacity):
+#         self.capacity = capacity  # max size it can be
+#         self.data = []  # holds the data
+#         self.cur = 0  # index of list
+
+#     def append(self, item):
+#         self.data.append(item)  # add item
+#         if len(self.data) == self.capacity:  # if at max capacity
+#             self.data[self.cur] = item  # replace with item
+#             # next will replace next index of list
+#             self.cur = (self.cur+1) % self.capacity
+
+#     def get(self):
+#         return self.data  # return the list of elements of not full
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity  # max size it can be
@@ -8,7 +23,7 @@ class RingBuffer:
         if len(self.data) == self.capacity:  # if at max capacity
             # replace node at index starting with 0 until end (self.cur is current index)
             self.data[self.cur] = item
-            if self.cur >= self.capacity:
+            if self.cur + 1 == self.capacity:
                 self.cur = 0
             else:
                 self.cur += 1
