b5ce093a1a6177f9989832377e17e4f9b3c5ed5b
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-08-14 14:22:22 -0500

    working on names because i forgot about it

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,18 +1,3 @@
-# class RingBuffer:
-#     def __init__(self, capacity):
-#         self.capacity = capacity  # max size it can be
-#         self.data = []  # holds the data
-#         self.cur = 0  # index of list
-
-#     def append(self, item):
-#         self.data.append(item)  # add item
-#         if len(self.data) == self.capacity:  # if at max capacity
-#             self.data[self.cur] = item  # replace with item
-#             # next will replace next index of list
-#             self.cur = (self.cur+1) % self.capacity
-
-#     def get(self):
-#         return self.data  # return the list of elements of not full
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity  # max size it can be
