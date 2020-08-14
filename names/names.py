58c8b366e2ae08e64758a69a27dec3e95d6ce529
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-08-14 16:59:19 -0500

    adding more notes as I look up information to study MVP was completed earlier

--- names/names.py
+++ names/names.py
@@ -11,6 +11,20 @@ names_2 = set(f.read().split("\n"))  # List containing 10000 names
 f.close()
 
 # Return the list of duplicates in this data structure
+'''
+listName = [ variableName (declare first) for variableName in WhateverCheckingA if variableName in WhateverCheck2IcompareItto]
+
+my runtime was les than 1 second this way
+terminal after mulitple tries: 
+1. runtime: 0.007040739059448242 seconds
+2. runtime: 0.007941007614135742 seconds
+3. runtime: 0.0045588016510009766 seconds
+4. runtime: 0.005752086639404297 seconds
+5. runtime: 0.004940032958984375 seconds
+
+I guess this would be O(n) because it is searching and I think Sets are like Object is Javascript (at least it looks like it to me). 
+'''
+
 duplicates = [name for name in names_1 if name in names_2]
 
 # Replace the nested for loops below with your improvements
